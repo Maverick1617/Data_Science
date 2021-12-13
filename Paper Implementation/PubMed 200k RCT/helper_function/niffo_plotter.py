252d63649c1a98d1ccfe2242da485ddac02b5e36
niffoxic <niffoisme@gmail.com>
2021-12-13 23:59:12 +0530

    Updated helper_function

--- Paper Implementation/PubMed 200k RCT/helper_function/niffo_plotter.py
+++ Paper Implementation/PubMed 200k RCT/helper_function/niffo_plotter.py
@@ -1,6 +1,11 @@
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def plot_histogram(data, bins=5, xlabel="", ylabel="", title="", figsize=(12, 6)):
@@ -85,3 +90,45 @@ def plot_model_history_accuracy(history, figsize=(10, 6), model_name="Model"):
 
     plt.legend(facecolor="#FAFAFA")
     plt.show()
+
+
+def plot_confusion_matrix(y_true, y_predict, classes=None, figsize=(10, 10), text_size=15, norm=False,
+                          fontsize=20):
+
+    # Create the confusion matrix
+    sns.set_style("whitegrid", {'axes.grid': False})
+    cm = confusion_matrix(y_true, y_predict)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+    n_classes = cm.shape[0]
+    fig, ax = plt.subplots(figsize=figsize)
+    cax = ax.matshow(cm, cmap="Reds")
+    fig.colorbar(cax)
+
+    if classes:
+        labels = classes
+    else:
+        labels = np.arange(cm.shape[0])
+
+    ax.set(title="Confusion Matrix",
+           ylabel="True label",
+           xlabel="Predicted label",
+           xticks=np.arange(n_classes),
+           yticks=np.arange(n_classes),
+           xticklabels=labels,
+           yticklabels=labels)
+
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+    threshold = (cm.max() + cm.min()) / 2.
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if norm:
+            plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > threshold else "black",
+                     size=text_size)
+        else:
+            plt.text(j, i, f"{cm[i, j]}",
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > threshold else "black",
+                     size=text_size)
